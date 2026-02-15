92ad3beec4bea51cedb95e27c1175b1fe1524da6
Asmaa D. Almadhoun <eng.asmaa.66@gmail.com>
2026-02-15 14:43:58 +0800

    Add files via upload

---
+++ src/resources/arabic/ar.i18next.js
@@ -0,0 +1,181 @@
+import React from "react";
+
+let ArabicTranslation;
+ArabicTranslation = {
+  local: 'ar',
+  shared: {
+    add: 'إضافة',
+    send: 'إرسال',
+    edit: 'تعديل',
+    remove: 'حذف',
+    reject: 'رفض',
+    accept: 'قبول',
+    save: 'حفظ',
+    cancel: 'الغاء',
+    update: 'تحديث',
+    empty: 'لا يوجد بيانات',
+    breakday: 'يوم إستراحة',
+    errors: {
+      notFoundUser: 'مستخدم غير موجود',
+      globalError: 'لقد حدث خطأ يرجى المحاولة ثانية'
+    },
+    success: {
+      accountCreated: 'تم إنشاء الحساب بنجاح',
+      deletedSuccess: 'تم الحذف بنجاح',
+      addedSuccess: 'تم الإضافة بنجاح',
+
+    },
+    warnings: {
+      emailExistBefore: 'البريد الألكترونى مستخدم من قبل'
+    }
+  },
+  breadcrumb: {
+    overview: 'عام',
+    request: 'الطلبات',
+    profile: 'الصفحة الشخصية',
+    setting: 'ضبط',
+    templates: 'نماذج',
+    inviteTrainee: 'دعوة متدرب'
+  },
+  login: {
+    title: 'تسجيل دخول',
+    userName: 'أسم المستخدم',
+    password: 'كلمة المرور',
+    rememberMe: 'تذكرنى',
+    forgetPW: 'هل نسيت كلمة المرور ؟',
+    newTrainer: 'مدرب جديد ؟',
+    signUp: 'الأشتراك'
+  },
+  register: {
+    title: 'التسجيل',
+    email: 'البريد الألكترونى',
+    fullName: 'الأسم بالكامل',
+    hasAccount: 'هل تمتلك حساب ؟',
+    miranTrainer: 'مدرب',
+    sasTrainer: 'مدرب SaaS',
+    experienceYear: 'سنوات الخبرة',
+    phoneNumber: 'رقم الهاتف',
+    nationality: 'الجنسية',
+    gender: 'الجنس',
+    male: 'ذكر',
+    female: 'أنثى',
+    languages: 'اللغات',
+    lang: 'اللغة',
+    aboutMe: 'نبذه عنى',
+    profileImg: 'الصورة الشخصية',
+    addDoc: 'أضافة مستندات',
+    next: 'التالى',
+    previous: 'السابق'
+  },
+  forgetPwPage: {
+    success: "تم ارسال رابط تغيير كلمة المرور على البريد الألكترونى",
+    failed: "البريد الألكترونى غير موجود"
+  },
+  menu: {
+    dashboard: 'لوحة التحكم',
+    chats: 'المحادثات',
+    addUser: 'اضافة متدرب',
+    trainees: 'المتدربين',
+    profile: 'الصفحة الشخصية',
+    templates: 'نماذج',
+    settings: 'الضبط',
+    logout: 'تسجيل الخروج'
+  },
+  header: {
+    searchTitle: 'كلمة البحث',
+    trainer: 'مدرب',
+  },
+  request: {
+    request: 'الطلبات',
+    viewRequest: 'عرض الطلب',
+    onHold: 'بانتظار القبول',
+    newRequest: 'طلبات جديدة',
+    invitationsTrainees: 'دعوات متدربين'
+  },
+  traineeModal: {
+    title: 'خطة',
+    Calories: 'السعرات الحرارية',
+    exercises: 'التمرين',
+    meals: 'الوجبات',
+    mealDetails: 'تفاصيل الوجبة',
+    calories: 'سعرات حراريه',
+    carbs: 'كربوهيدرات',
+    fat: 'دهون',
+    protein: 'بروتين',
+    totalDayIntakes: 'إجمالى ماأخذ اليوم',
+    totalTargetIntakes: 'إجمالى المستهدف',
+    addMeal: 'إضافة وجبة',
+    mealTitle: 'عنوان الوجبة',
+    breakDay: 'يوم عطلة',
+    copyMeal: 'نسخ الوجبة',
+    addExercise: 'تمرين',
+    copyExercise: 'نسخ تمرين',
+    searchFor: 'بحث',
+    gram: 'جم',
+    kGram: 'كجم',
+    remainingDays: 'أيام متبقية',
+    note: 'ملاحظة',
+    addNote: 'أضف ملاحظة',
+    notes: 'ملاحظات',
+    Reps: 'عدد العضلات',
+    rest: 'فترة الراحة',
+    sets: 'مجموعات',
+    titleCopyMeal: 'كم عدد الأيام المقترحة',
+    emptyDataExercise: 'هذا اليوم لا يحتوي على تمارين بعد',
+    emptyDataMeal: 'هذا اليوم لا يحتوي على وجبات بعد'
+  },
+  profile: {
+    fullName: 'الإسم بالكامل',
+    email: 'البريد الإلكترونى',
+    tele: 'الجوال',
+    gender: 'النوع',
+    country: 'الدولة',
+    aboutMe: 'نبذة عني',
+    BirthDate: 'تاريخ الميلاد',
+    nationality: 'الجنسية',
+    height: 'الطول',
+    levelOfActivity: 'مرحلة النشاط',
+    warning: 'التحذير',
+    Allergies: 'الحساسية',
+    myGoal: 'أهدافي'
+  },
+  settingPage: {
+    language: 'اللغة',
+    receiveTrainingRequest: 'استقبال طلبات التدريب',
+    receiveRenewalRequest: 'استقبال طلبات التجديد',
+    notification: 'التنبيهات'
+  },
+  templatePage: {
+    assign: 'تعيين',
+    add: 'اضافة نموذج',
+    numberOf: 'أضف رقم',
+    comments: 'تعليق',
+    templateName: 'اسم القالب',
+    chooseTrainee: 'اختر المدرب',
+    date: 'التاريخ',
+    addFood: 'أضف طعام'
+  },
+  faqPage: {
+    faq: 'أسئلة شائعة',
+    addQuestion: 'إضافة سؤال',
+    question: 'السؤال',
+    answer: 'الإجابة',
+  },
+  progressPage: {
+    progress: 'المتابعة',
+    measurement: 'القياسات',
+    weight: 'الوزن',
+    water: 'الماء',
+    picture: 'الصور',
+    current: 'الحالي',
+    target: 'الهدف',
+    day: 'اليوم',
+    value: 'القيمة',
+    change: 'التغيير',
+    weightChanges: 'تغيير الوزن',
+    changesWaist: 'تغييرات الخصر'
+  }
+
+};
+
+  export default ArabicTranslation;
---
+++ src/resources/engish/en.i18next.js
@@ -0,0 +1,178 @@
+const EnglishTranslation = {
+    local:'en',
+
+    shared : {
+        add:'Add',
+        edit:'Edit',
+        remove: 'Remove',
+        send  : 'Send',
+        save:'Save',
+        cancel:'Cancel',
+        reject: 'Reject',
+        accept: 'Accept',
+        update:'Update',
+        empty: 'Empty Data',
+        breakday: 'Break Day',
+        errors: {
+            notFoundUser : 'User Not Found',
+            globalError : 'Error has been happened! try again'
+        },
+        success : {
+            accountCreated : 'Account is created Successfully',
+            deletedSuccess : 'Deleted Successfully !',
+            addedSuccess : 'Added Successfully !'
+        },
+        warnings : {
+            emailExistBefore : 'email address already exists'
+        }
+    },
+    breadcrumb :  {
+        overview : 'Overview',
+        request : 'Request',
+        profile : 'Profile',
+        setting:'Settings',
+        templates: 'Templates',
+        inviteTrainee : 'Invite Trainee'
+    },
+    login: {
+        title:'Log in',
+        userName : 'Username',
+        password:'Password',
+        rememberMe : ' Remember me',
+        forgetPW: 'Forget Password ?',
+        newTrainer:'New Trainer ?',
+        signUp :'Sign up'
+    },
+    register : {
+        title:'Sign up',
+        email : 'Email',
+        fullName : 'Full Name',
+        hasAccount : 'Do you an account ?',
+        miranTrainer : 'Trainer',
+        sasTrainer : 'SAS Trainer',
+        experienceYear : 'Experience Year',
+        phoneNumber : 'Phone Number',
+        nationality : 'Nationality',
+        gender:'Gender',
+        male:'Male',
+        female:'Female',
+        languages : 'Languages',
+        lang : 'Language',
+        aboutMe: 'About me' ,
+        profileImg:'Profile Image',
+        addDoc:'Add Documents',
+        next : 'Next',
+        previous:'Previous',
+    },
+    forgetPwPage : {
+        success : "A reset link has been sent to your email",
+        failed: "User with that email does not exist"
+    },
+    menu : {
+        dashboard:'Dashboard',
+        chats:'Chats',
+        addUser : 'Add Trainee',
+        trainees: 'Trainees',
+        profile:'Profile',
+        templates:'Templates',
+        settings:'Settings',
+        logout:'Logout'
+    },
+    header : {
+        searchTitle : 'Search Message',
+        trainer:'Trainer',
+    },
+    request: {
+        request: 'Requests',
+        viewRequest: 'View requests',
+        onHold: 'On Hold',
+        newRequest: 'New Requests',
+        invitationsTrainees: 'Invitations Trainees'
+    },
+    traineeModal : {
+        title:'Plan',
+        Calories:'Calories Goal',
+        exercises: 'Exercises',
+        meals : 'Meals',
+        mealDetails:'Meal Details',
+        calories:'Calories',
+        carbs:'Carbs',
+        fat:'Fat',
+        protein: 'Protein',
+        totalDayIntakes : 'Total Day Intakes',
+        totalTargetIntakes:  'Total Target Intakes',
+        addMeal : 'Add Meal',
+        mealTitle : 'Meal Title',
+        breakDay : 'Break Day',
+        copyMeal: 'Copy Meal',
+        addExercise:'Add Exer',
+        copyExercise: 'Copy Exer',
+        searchFor: 'Search',
+        gram: 'g',
+        kGram: 'Kg',
+        remainingDays: 'days remaining',
+        note: 'Note',
+        addNote: 'Add Note',
+        notes: 'Notes',
+        rest: 'Rest period',
+        Reps: 'Reps',
+        sets: 'Sets',
+        titleCopyMeal: 'How many days should be?',
+        emptyDataExercise: 'This day has no exercises yet',
+        emptyDataMeal: 'This day has no Meals yet'
+    },
+    profile : {
+        fullName : 'Full Name',
+        email : 'Email' ,
+        tele : 'Telephone',
+        gender : 'Gender',
+        country : 'Country',
+        aboutMe : 'About me',
+        BirthDate: 'Date of Birth',
+        nationality: 'Nationality',
+        height: 'Height',
+        levelOfActivity: 'Level Of Activity',
+        warning: 'Warning',
+        Allergies: 'Allergies',
+        myGoal: 'My Goal'
+    },
+    settingPage : {
+        language : 'Language',
+        receiveTrainingRequest: 'Receive Training Requests',
+        receiveRenewalRequest : 'Receive Renewal Requests',
+        notification : 'Notifications'
+    },
+    templatePage : {
+        assign : 'Assign',
+        add : 'Add Template',
+        numberOf: 'Number of',
+        comments : 'Comment',
+        templateName: 'Template Name',
+        chooseTrainee: 'Choose Trainee',
+        date: 'Date',
+        addFood: 'Add Food'
+    },
+    faqPage: {
+        faq: 'FAQ',
+        addQuestion : 'Add Question',
+        question: 'Question',
+        answer: 'Answer',
+    },
+    progressPage: {
+        progress: 'Progress',
+        measurement: 'Measurements',
+        weight : 'Weight',
+        water: 'Water',
+        picture: 'Pictures',
+        current: 'Current',
+        target: 'Target',
+        day: 'Day',
+        value: 'Value',
+        change: 'Change',
+        weightChanges: 'Weight Changes',
+        changesWaist: 'Changes Waist'
+
+    }
+  };
+
+  export default  EnglishTranslation;
